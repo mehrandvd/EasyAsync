f2764f3b061c45c30f2960523e006ba2fdc09ce3
Mehran Davoudi <mehran.davoudi@hotmail.com>
2023-09-04 14:28:47 +0330

    Add Forget extension method.

---
+++ src/EasyAsync.Test/TaskExtensionsTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyAsync.Test
+{
+    public class TaskExtensionsTests
+    {
+        [Fact]
+        public async Task Forget_MustWork()
+        {
+            GetSampleAsync(1).Forget();
+        }
+
+        [Fact]
+        public async Task Forget_WithException_MustWork()
+        {
+            var isThrown = false;
+            ThrowAsync(1).Forget(onException: ()=>
+            {
+                isThrown = true;
+            });
+
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            Assert.True(isThrown);
+        }
+
+        [Fact]
+        public async Task Forget_WithCancellation_MustWork()
+        {
+            var isThrown = false;
+            var cts = new CancellationTokenSource();
+
+            ThrowAsync(2).Forget(onException: () =>
+            {
+                isThrown = true;
+            }, cancellationToken: cts.Token);
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            cts.Cancel();
+
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+
+            Assert.False(isThrown);
+        }
+
+        private async Task GetSampleAsync(int seconds)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(seconds));
+        }
+
+        private async Task ThrowAsync(int seconds)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(seconds));
+            throw new Exception("ERROR");
+        }
+    }
+}
---
+++ src/EasyAsync/TaskExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EasyAsync
+{
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Safely forget about a task when you do not want to await for it.
+        /// Although you don't want to await, but you can still handle its exceptions by setting its <param name="onException"></param>
+        /// </summary>
+        /// <param name="task"></param>
+        /// <param name="onException"></param>
+        /// <param name="cancellationToken"></param>
+        public static void Forget(this Task task, Action? onException = null, CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (onException != null)
+            {
+                task.ContinueWith(t =>
+                    {
+                        onException();
+                    },
+                    cancellationToken: cancellationToken,
+                    TaskContinuationOptions.OnlyOnFaulted, TaskScheduler.Current
+                );
+            }
+        }
+    }
+}
